df7175022264b765a6413776508c89ceec82664b
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-06-11 21:59:00 -0300

    hospeda video

--- src/app/fragments/hero/index.tsx
+++ src/app/fragments/hero/index.tsx
@@ -13,8 +13,17 @@ export default function Hero(): ReactNode {
 	return (
 		<>
 			<div id="hero">
-				<video autoPlay muted loop className="hero--video">
-					<source src="video.mp4" type="video/mp4" />
+				<video autoPlay muted loop className="hero--video desktop">
+					<source
+						src="https://ik.imagekit.io/ko20eqbppws/video.mp4"
+						type="video/mp4"
+					/>
+				</video>
+				<video autoPlay muted loop className="hero--video mobile">
+					<source
+						src="https://ik.imagekit.io/ko20eqbppws/vertical-video.mp4"
+						type="video/mp4"
+					/>
 				</video>
 				<div className="hero--content">
 					<Swiper
