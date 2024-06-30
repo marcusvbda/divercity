40b3d6a05d96a5eb62e583386a74920dec160820
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-06-30 13:23:41 -0300

    resolve redirect

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function Login() {
-	const searchParams = useSearchParams();
-	const callbackUrl = searchParams.get('callbackUrl') || '/admin';
+export default function Login({ searchParams }: any) {
+	const { callbackUrl } = searchParams;
 
 	useEffect(() => {
-		signIn('auth0', { callbackUrl });
+		signIn('auth0', { callbackUrl: callbackUrl || '/admin' });
 	}, [callbackUrl]);
 
 	return <>Redirecionando ...</>;
